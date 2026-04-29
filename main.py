8ca5a91b4a0544e2c68a77f577d81342a9f34ed5
seanmelo <smelone@protonmail.com>
2026-04-29 14:40:59 -0400

    working vault

--- main.py
+++ main.py
@@ -1,34 +1,6 @@
 # import os
 # from cryptography.fernet import Fernet
-
-
-class Vault:
-    def __init__(self):
-        self.table = {}
-
-    def add_entry(self, name: str, username: str, password: str) -> None:
-        if name in self.table:
-            print("Entry already exists. Overrwriting entry.")
-        self.table[name] = {"username": username, "password": password}
-
-    def remove_entry(self, name: str) -> None:
-        if name in self.table:
-            del self.table[name]
-            print(f"Successfully deleted {name} and associated data.")
-        else:
-            print("Record not found.")
-
-    def edit_password(self, name: str, password: str) -> None:
-        if name in self.table:
-            self.table[name]["password"] = password
-        else:
-            print("Record not found")
-
-    def edit_username(self, name: str, username: str) -> None:
-        if name in self.table:
-            self.table[name]["username"] = username
-        else:
-            print("Record not found")
+from vault import Vault
 
 
 def welcome() -> None:
@@ -37,6 +9,9 @@ def welcome() -> None:
 
 def main():
     welcome()
+    vault = Vault()
+    vault.add_entry("Google", "Joelikescybersecurity", "1337code")
+    print(vault.table)
 
 
 if __name__ == "__main__":
---
+++ vault.py
@@ -0,0 +1,27 @@
+class Vault:
+    def __init__(self):
+        self.table = {}
+
+    def add_entry(self, name: str, username: str, password: str) -> None:
+        if name in self.table:
+            print("Entry already exists. Overrwriting entry.")
+        self.table[name] = {"username": username, "password": password}
+
+    def remove_entry(self, name: str) -> None:
+        if name in self.table:
+            del self.table[name]
+            print(f"Successfully deleted {name} and associated data.")
+        else:
+            print("Record not found.")
+
+    def edit_password(self, name: str, password: str) -> None:
+        if name in self.table:
+            self.table[name]["password"] = password
+        else:
+            print("Record not found")
+
+    def edit_username(self, name: str, username: str) -> None:
+        if name in self.table:
+            self.table[name]["username"] = username
+        else:
+            print("Record not found")
