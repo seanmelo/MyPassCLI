e9c9878e1556edef052c3fbd85973e0caecb7ed1
seanmelo <smelone@protonmail.com>
2026-05-04 15:09:13 -0400

    MVP of main loop complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import os
 # from cryptography.fernet import Fernet
+# import json
 from vault import Vault
 
 
@@ -9,20 +10,41 @@ def welcome() -> None:
 
 def main():
     welcome()
+    # TODO: add json loading from local files and decryption
     vault = Vault()
     while True:
         choice = input(
-            "Enter 1 to add a login, 2 to fetch a login, any other input to save and quit"
+            "Enter 1 to add a login, 2 to fetch a login, 3 to edit a login, 4 to delete a login, any other input to save and quit: "
         )
         match choice:
-            case 1:
-                name = input("Enter website name.")
-                username = input(f"Enter username for {name}.")
-                password = input(f"Enter password for {name}.")
-                vault.add_entry(name, input, password)
-            case 2:
-                pass
+            case "1":
+                name = input("Enter website name: ")
+                username = input(f"Enter username for {name}: ")
+                password = input(f"Enter password for {name}: ")
+                vault.add_entry(name, username, password)
+            case "2":
+                name = input("Enter website name: ")
+                username, password = vault.get(name)
+                print(f"Username: {username}")
+                print(f"Password: {password}")
+            case "3":
+                name = input("Enter name of entry to edit: ")
+                new_username = input(
+                    f"Enter new username for {name}. Press ENTER to skip: "
+                )
+                if new_username:
+                    vault.edit_username(name, new_username)
+                new_password = input(
+                    f"Enter new password for {name}. Press ENTER to skip: "
+                )
+                if new_password:
+                    vault.edit_password(name, new_password)
+            case "4":
+                name = input("Enter website name to delete: ")
+                vault.remove_entry(name)
+
             case _:
+                # TODO: add json dumping and encrpytion
                 break
 
 
--- vault.py
+++ vault.py
@@ -12,16 +12,22 @@ class Vault:
             del self.table[name]
             print(f"Successfully deleted {name} and associated data.")
         else:
-            print("Record not found.")
+            raise Exception("Record not found.")
 
     def edit_password(self, name: str, password: str) -> None:
         if name in self.table:
             self.table[name]["password"] = password
         else:
-            print("Record not found")
+            raise Exception("Record not found.")
 
     def edit_username(self, name: str, username: str) -> None:
         if name in self.table:
             self.table[name]["username"] = username
         else:
-            print("Record not found")
+            raise Exception("Record not found.")
+
+    def get(self, name):
+        if name in self.table:
+            return self.table[name]["username"], self.table[name]["password"]
+        else:
+            raise Exception("Record not found.")
